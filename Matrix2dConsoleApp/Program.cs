7f876f486454b9d6e280ed1c9cb465387e345945
Artur Gorbachev <artur.gorbachev@microsoft.wsei.edu.pl>
2025-03-21 16:21:14 +0100

    update 5

--- Matrix2dConsoleApp/Program.cs
+++ Matrix2dConsoleApp/Program.cs
@@ -6,8 +6,31 @@ namespace Matrix2dConsoleApp
         static void Main(string[] args)
         {
             var m1 = new Matrix2d(1, 2, 3, 4);
-            var m2 = Matrix2d.Transpose(m1);
-            Console.WriteLine(m2.ToString());
+            Console.WriteLine($"=== Matrix 1, 2, 3, 4 ===\n{m1.ToString()}\n");
+            var m2 = new Matrix2d();
+            Console.WriteLine($"=== Matrix with empty constructor ===\n{m2.ToString()}\n");
+            Console.WriteLine($"=== Identity matrix ===\n{Matrix2d.Id.ToString()}\n");
+            Console.WriteLine($"=== Zero matrix ===\n{Matrix2d.Zero.ToString()}\n");
+            var m3 = new Matrix2d(1, 2, 3, 4);
+            Console.WriteLine($"=== Equals Method ===\n{m1.Equals(m3)}\n");
+            Console.WriteLine($"=== Matrix m1: {m1.ToString()} == m3: {m3.ToString()} ===\n{m1 == m3}\n");
+            Console.WriteLine($"=== Matrix m1: {m1.ToString()} != m2: {m2.ToString()} ===\n{m1 != m2}\n");
+            Console.WriteLine($"=== Matrix m1: {m1.ToString()} == m2: {m2.ToString()} ===\n{m1 == m2}\n");
+            Console.WriteLine($"=== Matrix m1: {m1.ToString()} != m3: {m3.ToString()} ===\n{m1 != m3}\n");
+            Console.WriteLine($"=== Matrix m1: {m1.ToString()} + m2: {m2.ToString()} ===\n{m1 + m2}\n");
+            Console.WriteLine($"=== Matrix m1: {m1.ToString()} - m2: {m2.ToString()} ===\n{m1 - m2}\n");
+            Console.WriteLine($"=== Matrix m1: {m1.ToString()} * m2: {m2.ToString()} ===\n{m1 * m2}\n");
+            Console.WriteLine($"=== Matrix m1: {m1.ToString()} * m3: {m3.ToString()} ===\n{m1 * m3}\n");
+            Console.WriteLine($"=== -m1 ===\n{-m1}\n");
+            int[,] int1 = (int[,])m1;
+            Console.WriteLine($"=== Conversion from Matrix2d to int[,] ===\n[{int1[0, 0]}, {int1[0, 1]}], [{int1[1, 0]}, {int1[1, 1]}]\n");
+            Matrix2d m4 = int1;
+            Console.WriteLine($"=== Conversion from int[,] to Matrix2d ===\n{m4.ToString()}\n");
+            var m5 = Matrix2d.Parse("[5, 6], [7, 8]");
+            Console.WriteLine($"=== Parse Method ===\n{m5.ToString()}\n");
+            var m6 = Matrix2d.Transpose(m5);
+            Console.WriteLine($"=== Transpose Method ===\n{m6.ToString()}\n");
+            Console.WriteLine($"=== Determinant Method ===\nm6.Det: {m6.Det()}\nMatrix2d.Determinant(m6): {Matrix2d.Determinant(m6)}\n");
         }
     }
 }
--- Matrix2dLib/Matrix2d.cs
+++ Matrix2dLib/Matrix2d.cs
@@ -1,12 +1,9 @@
 ﻿#nullable enable
 namespace Matrix2dLib;
 
-//immutable 2x2 matrix
 public class Matrix2d
 {
     private int _a, _b, _c, _d;
-
-    //constructor
     public Matrix2d(int a, int b, int c, int d)
     {
         _a = a;
@@ -15,18 +12,12 @@ public class Matrix2d
         _d = d;
     }
     public Matrix2d() : this(1, 0, 0, 1) { }
-
-    //properties
     public int A => _a;
     public int B => _b;
     public int C => _c;
     public int D => _d;
-
     public static Matrix2d Id => new Matrix2d();
-
     public static Matrix2d Zero => new Matrix2d(0, 0, 0, 0);
-
-    //methods
     override public string ToString() => $"[{_a}, {_b}], [{_c}, {_d}]";
 
     public bool Equals(Matrix2d other)
